35f360f8317e734b1224cb90770cdc78692067d2
nerdgrass <arboerstler@gmail.com>
2015-01-08 12:33:13 -0500

    updated scaffolding, images CSS and watch tasks in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -26,3 +26,38 @@ var deploy = require('gulp-gh-pages'),
     uglify = require('gulp-uglify'),
     htmlmin = require('gulp-htmlmin'),
     cssmin = require('gulp-minify-css');
+
+
+//
+// Super Happy Funtime Build System Extraordinaire
+// 
+
+// Watch task (runs default on changes, runs LR)
+gulp.task('watch', ['default'], function () {
+  gulp.watch(['app/templates/**'], ['content'])
+  gulp.watch(['app/styles/**'], ['styles'])
+  gulp.watch(['app/images/**'], ['images'])
+  gulp.watch(['app/posts/**'], ['posts', 'rss'])
+
+  scripts(true)
+  // LR crap
+  connect.server({
+    root: ['dist'],
+    port: 9000,
+    livereload: true
+  })
+})
+
+// Styles task (compiles Sass)
+gulp.task('styles', function () {
+  gulp.src('app/styles/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css/app.css'));
+});
+
+// Images task (Moves images & media to /dist)
+gulp.task('images', function () {
+  return gulp.src('app/images/**')
+    .pipe(gulp.dest('dist/images'))
+    .pipe(connect.reload())
+})
