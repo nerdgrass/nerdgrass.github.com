5da145f4d0600fbf9948d4126e7e9ca46e4855aa
nerdgrass <arboerstler@gmail.com>
2015-01-10 14:24:13 -0500

    SCSS, HTML, connect working

--- gulpfile.js
+++ gulpfile.js
@@ -14,9 +14,9 @@ var gulp = require('gulp'),
 
 // Modules for compiling/transpiling
 var sass = require('gulp-sass'),
-    jade = require('gulp-jade'),
-    marked = require('gulp-marked'),
-    frontMatter = require('gulp-front-matter'),
+    // jade = require('gulp-jade'),
+    // marked = require('gulp-marked'),
+    // frontMatter = require('gulp-front-matter'),
     browserify = require('browserify'),
     streamify = require('gulp-streamify'),
     watchify = require('watchify');
@@ -28,28 +28,32 @@ var deploy = require('gulp-gh-pages'),
     cssmin = require('gulp-minify-css');
 
 // Site info
-var site = {
-    'title': 'Alex Boerstler Design &amp; Development',
-    'url': 'http://localhost:9000',
-    'urlRoot': '/',
-    'author': 'Alex Boerstler',
-    'email': 'alex@nerdgrass.com',
-    'time': new Date()
-}
+// var site = {
+//     'title': 'Alex Boerstler Design &amp; Development',
+//     'url': 'http://localhost:9000',
+//     'urlRoot': '/',
+//     'author': 'Alex Boerstler',
+//     'email': 'alex@nerdgrass.com',
+//     'time': new Date()
+// }
 
 
 //
 // Super Happy Funtime Build System Extraordinaire
 // 
 
+// Default Task
+gulp.task('default', ['images', 'styles', 'favicon', 'html'])
+
 // Watch task (runs default on changes, runs LR)
 gulp.task('watch', ['default'], function () {
-  gulp.watch(['app/templates/**'], ['templates'])
+  // gulp.watch(['app/templates/**'], ['templates'])
   gulp.watch(['app/styles/**'], ['styles'])
   gulp.watch(['app/images/**'], ['images'])
-  gulp.watch(['app/posts/**'], ['posts', 'rss'])
+  gulp.watch(['app/index.html'], ['html'])
+  // gulp.watch(['app/posts/**'], ['posts', 'rss'])
 
-  scripts(true)
+  // scripts(true)
   // LR crap
   connect.server({
     root: ['dist'],
@@ -60,15 +64,22 @@ gulp.task('watch', ['default'], function () {
 
 // Styles task (compiles Sass)
 gulp.task('styles', ['cleanstyles'], function () {
-  gulp.src('app/styles/scss/*.scss')
+  gulp.src('app/styles/*.scss')
     .pipe(sass())
-    .pipe(gulp.dest('dist/css/app.css'));
+    .pipe(gulp.dest('dist/styles'));
 });
 
 // Images task (Moves images & media to /dist)
 gulp.task('images', function () {
   return gulp.src('app/images/**')
     .pipe(gulp.dest('dist/images'))
+    .pipe(connect.reload())
+})
+
+// HTML task (Moves index.html to /dist)
+gulp.task('html', function () {
+  return gulp.src('app/index.html')
+    .pipe(gulp.dest('dist/'))
     .pipe(connect.reload())
 })
 
@@ -80,85 +91,85 @@ gulp.task('favicon', ['cleanfavicon'], function () {
 })
 
 // Scripts task
-function scripts(watch) {
-  var args = watch ? _.clone(watchify.args) : {}
-  args.debug = DEBUG
+// function scripts(watch) {
+//   var args = watch ? _.clone(watchify.args) : {}
+//   args.debug = DEBUG
 
-  var bundler = browserify('./app/scripts/app.js', args)
+//   var bundler = browserify('./app/scripts/app.js', args)
 
-  if (watch) {
-    bundler = watchify(bundler)
-  }
+//   if (watch) {
+//     bundler = watchify(bundler)
+//   }
 
-  function rebundle() {
-    gutil.log('Bundling... ')
+//   function rebundle() {
+//     gutil.log('Bundling... ')
 
-    return bundler.bundle()
-      // log errors if they happen
-      .on('error', function(e) {
-        gutil.log('Browserify Error', e)
-      })
-      .pipe(source('app.js'))
-      .pipe(gulpif(!DEBUG, streamify(uglify())))
-      .pipe(gulp.dest('./dist/scripts'))
-      .pipe(connect.reload())
-  }
+//     return bundler.bundle()
+//       // log errors if they happen
+//       .on('error', function(e) {
+//         gutil.log('Browserify Error', e)
+//       })
+//       .pipe(source('app.js'))
+//       .pipe(gulpif(!DEBUG, streamify(uglify())))
+//       .pipe(gulp.dest('./dist/scripts'))
+//       .pipe(connect.reload())
+//   }
 
-  bundler.on('update', rebundle)
-  return rebundle()
-}
+//   bundler.on('update', rebundle)
+//   return rebundle()
+// }
 
 // Templates task
-gulp.task('templates', function() {
-  var YOUR_LOCALS = {};
+// gulp.task('templates', function() {
+//   var YOUR_LOCALS = {};
 
-  gulp.src('app/templates/*.jade')
-    .pipe(jade({
-      locals: YOUR_LOCALS
-    }))
-    .pipe(gulp.dest('dist/'))
-});
+//   gulp.src('app/templates/*.jade')
+//     .pipe(jade({
+//       locals: YOUR_LOCALS
+//     }))
+//     .pipe(gulp.dest('dist/'))
+// });
 
 
 // Posts task
-gulp.task('posts', ['cleanposts'], function () {
-  return gulp.src('app/posts/*.md')
-    .pipe(frontMatter({property: 'page', remove: true}))
-    .pipe(marked())
-    .pipe(summarize('<!--more-->'))
-    // Collect all the posts and place them on the site object.
-    .pipe((function () {
-      var posts = []
-      var tags = []
-      return through.obj(function (file, enc, cb) {
-        file.page.url = 'posts/' + path.basename(file.path, '.md')
-        posts.push(file.page)
-        posts[posts.length - 1].content = file.contents.toString()
+// gulp.task('posts', ['cleanposts'], function () {
+//   return gulp.src('app/posts/*.md')
+//     .pipe(frontMatter({property: 'page', remove: true}))
+//     .pipe(marked())
+//     .pipe(summarize('<!--more-->'))
+//     // Collect all the posts and place them on the site object.
+//     .pipe((function () {
+//       var posts = []
+//       var tags = []
+//       return through.obj(function (file, enc, cb) {
+//         file.page.url = 'posts/' + path.basename(file.path, '.md')
+//         posts.push(file.page)
+//         posts[posts.length - 1].content = file.contents.toString()
 
-        if (file.page.tags) {
-          file.page.tags.forEach(function (tag) {
-            if (tags.indexOf(tag) === -1) {
-              tags.push(tag)
-            }
-          })
-        }
+//         if (file.page.tags) {
+//           file.page.tags.forEach(function (tag) {
+//             if (tags.indexOf(tag) === -1) {
+//               tags.push(tag)
+//             }
+//           })
+//         }
 
-        this.push(file)
-        cb()
-      },
-      function (cb) {
-        posts.sort(function (a, b) {
-          return b.date - a.date
-        })
-        site.posts = posts
-        site.tags = tags
-        cb()
-      })
-    })())
-    .pipe(applyTemplate('app/templates/post.html'))
-    .pipe(gulp.dest('dist/posts'))
-    .pipe(connect.reload())
-})
+//         this.push(file)
+//         cb()
+//       },
+//       function (cb) {
+//         posts.sort(function (a, b) {
+//           return b.date - a.date
+//         })
+//         site.posts = posts
+//         site.tags = tags
+//         cb()
+//       })
+//     })())
+//     .pipe(applyTemplate('app/templates/post.html'))
+//     .pipe(gulp.dest('dist/posts'))
+//     .pipe(connect.reload())
+// })
 
 // Cleaning Tasks
 gulp.task('clean', function() {
@@ -173,10 +184,10 @@ gulp.task('cleanscripts', function () {
   return gulp.src(['dist/scripts'], {read: false})
     .pipe(rimraf())
 })
-gulp.task('cleanposts', function () {
-  return gulp.src(['dist/posts'], {read: false})
-    .pipe(rimraf())
-})
+// gulp.task('cleanposts', function () {
+//   return gulp.src(['dist/posts'], {read: false})
+//     .pipe(rimraf())
+// })
 gulp.task('cleanimages', function () {
   return gulp.src(['dist/images'], {read: false})
     .pipe(rimraf())
