55ec6f1f80774c72d6a8b554cb0c812a58471394
Alex B <arboerstler@gmail.com>
2017-02-26 10:01:46 -0500

    adds pages folder, scaffolds homepage

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
+// Dependencies
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
+// Pages
 import App from './components/App';
-import Home from './components/Home';
+import Home from './pages/home';
 import PageNotFound from './components/PageNotFound';
 import ExampleComponent from './components/ExampleComponent';
 import ExampleTwoDeepComponent from './components/ExampleTwoDeepComponent';
 
+// Dummy Page Components - Remove before publishing
+const DummyPage = ({ msg }) => <h1>{msg}</h1>;
+const DummyAbout = (props) => <DummyPage msg="About" />;
+const DummyWork = (props) => <DummyPage msg="Work" />;
+const DummyContact = (props) => <DummyPage msg="Contact" />;
 
 const routes = (
-  <Route path="/" mapMenuTitle="Home" component={App}>
+  <Route path="/" mapMenuTitle="Home">
     <IndexRoute component={Home} />
-
-    <Route path="example" mapMenuTitle="Example" component={ExampleComponent}>
-      <Route path="two-deep" mapMenuTitle="Two Deep" component={ExampleTwoDeepComponent} />
-    </Route>
-
+    <Route path="about" mapMenuTitle="About Me" component={DummyAbout} />
+    <Route path="work" mapMenuTitle="Work" component={DummyWork} />
+    <Route path="contact" mapMenuTitle="Contact" component={DummyContact} />
     <Route path="*" mapMenuTitle="Page Not Found" component={PageNotFound} />
   </Route>
 );
-
 
 render(
   <Router
---
+++ src/pages/home.js
@@ -0,0 +1,33 @@
+// Dependencies
+import React, { PropTypes } from 'react';
+
+// Components
+
+// Page
+const Home = (props) => {
+  return (
+    <div>
+      <div className="splash">
+        <h1>Flourish Goes Here</h1>
+      </div>
+      <div className="navbar">
+        <h1>Navbar goes here</h1>
+      </div>
+      <div className="workLinks">
+        <h1>workLinks go here</h1>
+      </div>
+      <div className="whoIAm">
+        <h1>who I am goes here </h1>
+      </div>
+      <div className="footer">
+        <h1>Footer goes here</h1>
+      </div>
+    </div>
+  );
+};
+
+Home.PropTypes = {
+
+};
+
+export default Home;
